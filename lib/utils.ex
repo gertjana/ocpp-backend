0886b0d16682aae24c967944fb9b609ad1daa008
Gertjan Assies <g.assies@thenewmotion.com>
2017-09-12 12:34:00 +0200

    formatting

--- lib/utils.ex
+++ lib/utils.ex
@@ -1,26 +1,33 @@
 defmodule Utils do
   use Timex
 
+  @spec time_as_string() :: String.t()
   def time_as_string do
     {hh, mm, ss} = :erlang.time()
     :io_lib.format("~2.10.0B:~2.10.0B:~2.10.0B", [hh, mm, ss])
     |> :erlang.list_to_binary()
   end
 
+  @spec datetime_as_string() :: String.t()
   def datetime_as_string do
-  	{:ok, dt_string} = Timex.format(Timex.now, "{ISO:Extended}")
-  	dt_string
+    {:ok, result} = Timex.now 
+                    |> Timex.format("{ISO:Extended}")
+    result 
   end
 
+  @spec datetime_as_string(integer) :: String.t()
   def datetime_as_string(shift_minutes) do
-    {:ok, dt_string} = Timex.format(Timex.shift(Timex.now, minutes: shift_minutes), "{ISO:Extended}")
-  	dt_string
+    {:ok, result} = Timex.now 
+                    |> Timex.shift(minutes: shift_minutes)
+                    |> Timex.format("{ISO:Extended}")
+  	result
   end
 
-  def default(val, default) do
-    case val do
-      nil -> default
-      _ -> val
+  @spec default(any, any) :: any
+  def default(value, default_value) do
+    case value do
+      nil -> default_value
+      _ -> value
     end
   end
 
